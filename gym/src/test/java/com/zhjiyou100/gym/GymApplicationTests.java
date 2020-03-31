b6ad3fb7e356a443b8ad657018cec0611dcb19c8
feng970305 <feng970305@163.com>
2020-03-31 11:28:42 +0800

    上传项目

---
+++ gym/src/test/java/com/zhjiyou100/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhjiyou100.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
